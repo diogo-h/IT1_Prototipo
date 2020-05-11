222a1e136d8656362f94656a28427678ad636e24
diogo-h <diogo.m.henriques98@gmail.com>
2020-05-11 11:54:06 +0100

    Setup Project and initial movement control

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Variables
+    private CharacterController controller;
+    private Vector3 direction;
+    private Vector3 targetPosition;
+    private int desiredLane = 0;
+    [SerializeField] private float laneDistance = 4;
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        direction.z = forwardSpeed;
+        direction.y += gravity*Time.deltaTime;
+        // Movement
+        if(Input.GetKeyDown(KeyCode.D)) desiredLane++;
+        if(Input.GetKeyDown(KeyCode.A)) desiredLane--;
+        if(controller.isGrounded)
+        {
+            direction.y = -1;
+            if(Input.GetKeyDown(KeyCode.Space)) Jump();
+        }
+        else direction.y += gravity * Time.deltaTime;
+
+
+        // Future Position
+        targetPosition = transform.position.z * transform.forward + 
+            transform.position.y * transform.up;
+
+        if(desiredLane == 0) targetPosition += Vector3.left * laneDistance;
+        else if (desiredLane == 1) targetPosition += Vector3.right * laneDistance;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 80 * Time.deltaTime);
+
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        direction.y = jumpForce;
+    }
+}
